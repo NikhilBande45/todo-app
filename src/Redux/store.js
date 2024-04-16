ee56ab1ecaeb0d4e62fb4bb73287be8d64c9fa87
NikhilBande45 <bandenikhil45@gmail.com>
2024-04-16 23:13:35 +0530

    resolved deploy issue

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux'
 
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import todoReducer from './reducers'
 
-const store = createStore(todoReducer , composeWithDevTools())
+const store = createStore(todoReducer)
 
 export default store;
