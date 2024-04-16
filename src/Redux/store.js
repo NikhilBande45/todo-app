092fa4f19a222814339c376d5eeec8c354a7984b
NikhilBande45 <bandenikhil45@gmail.com>
2024-04-16 23:19:21 +0530

    resolved redux store issue

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,5 @@
 import { legacy_createStore } from 'redux'
 
-// import { composeWithDevTools } from 'redux-devtools-extension';
 import todoReducer from './reducers'
 
 const store = legacy_createStore(todoReducer)
